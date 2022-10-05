1b5fd2c4eeb0bd8242fa2aa8d725d4ab4034276a
unknown <david.otalora-b@labinfo.is.escuelaing.edu.co>
2022-10-05 12:44:00 -0500

    first update

---
+++ src/main/java/co/edu/escuelaing/sparkdockerdemolive/MongoDB.java
@@ -0,0 +1,4 @@
+package co.edu.escuelaing.sparkdockerdemolive;
+
+public class MongoDB {
+}
--- src/main/java/co/edu/escuelaing/sparkdockerdemolive/SparkWebServer.java
+++ src/main/java/co/edu/escuelaing/sparkdockerdemolive/SparkWebServer.java
@@ -2,12 +2,17 @@ package co.edu.escuelaing.sparkdockerdemolive;
 
 import static spark.Spark.get;
 import static spark.Spark.port;
-
+//co\edu\escuelaing\sparkdockerdemolive\SparkWebServer.java
 public class SparkWebServer {
 
-    //src/main/java/co/edu/escuelaing/sparkdockerdemolive/SparkWebServer.java
+    //
+    //server.port=8081
+    //
+    //mongodb.database=tasks
+    //mongodb.connection.string=mongodb://localhost:27017
     public static void main(String... args){
         port(getPort());
+        System.out.println(getPort());
         get("hello", (req,res) -> "Hello Docker!");
     }
 
